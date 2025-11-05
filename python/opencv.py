c54f708d85d3fb8a135cfc73dfd67b772be7d614
Liutprando08 <liutprando.federico@protonmail.com>
2025-11-05 21:29:19 +0100

    migliorare open.cv

--- python/opencv.py
+++ python/opencv.py
@@ -3,6 +3,52 @@ import pytesseract
 import tkinter as tk
 from tkinter import filedialog
 
+
+def preprocess_image(image_path):
+    img = cv2.imread(image_path)
+    if img is None:
+        raise FileNotFoundError("Immagine non trovata")
+
+    # conversione il scala di grigi
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # riduzione del rumore
+    gray = cv2.medianBlur(gray, 3)
+
+    # BInarizzazione con Otsu
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # Morfologia per pulizia piccoli artefatti
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 1))
+    processed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+    # allineamento testo
+    coords = cv2.findNonZero(processed)
+    if coords is not None:
+        angle = cv2.minAreaRect(coords)[-1]
+        if angle < -45:
+            angle = -(90 + angle)
+        else:
+            angle = -angle
+
+        (h, w) = processed.shape[:2]
+        center = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated = cv2.warpAffine(
+            processed, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE
+        )
+    else:
+        rotated = processed
+
+    return rotated
+
+
+def extract_text(image_path, lang="ita"):
+    processed_img = preprocess_image(image_path)
+    text = pytesseract.image_to_string(processed_img, lang=lang)
+    return text
+
+
 root = tk.Tk()
 root.withdraw()
 print("seleziona file  per opencv1")
@@ -10,7 +56,12 @@ file_path = filedialog.askopenfilename(
     title="seleziona un immagine",
     filetypes=[("Immagini", "*.png *.jpg *.jpeg *.bmp *.tiff")],
 )
-if file_path:
-    print(f"hai selezionato: {file_path}")
-else:
-    print("nessun file selezionato")
+if not file_path:
+    print("Nessun file selezionato. Uscita...")
+    exit()
+try:
+    text = extract_text(file_path)
+    print("\n--- Testo estratto ---\n")
+    print(text)
+except FileNotFoundError as e:
+    print(e)
