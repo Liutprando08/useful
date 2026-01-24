55e7934299d4541667e893c0e257ad6adcd1d8d2
Liutprando08 <liutprando.federico@protonmail.com>
2026-01-24 20:20:47 +0100

    mirrornumber

---
+++ c/mirrorNumber.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void) {
+  int num, square, resto1, resto2, specchio1, specchio2, quadrato, radice;
+  int EXIT_SUCCESS = 0;
+  specchio1 = 0;
+  specchio2 = 0;
+
+  printf("scrivi il numero per vedere se si puo invertire");
+  scanf("%d", &num);
+
+  quadrato = pow(num, 2);
+  while (quadrato != 0) {
+    resto1 = quadrato % 10;
+    specchio1 = specchio1 * 10 + resto1;
+    quadrato = quadrato / 10;
+  }
+  radice = sqrt(specchio1);
+
+  while (radice != 0) {
+    resto2 = radice % 10;
+    specchio2 = specchio2 * 10 + resto2;
+    radice = radice / 10;
+  }
+  if (specchio2 == num) {
+    printf("e uno specchio");
+  } else {
+    printf("non e uno specchio");
+  }
+  return EXIT_SUCCESS;
+}
