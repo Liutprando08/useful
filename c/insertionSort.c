f64019849c58a9452b68aa5a67f6af3b3d6972d8
Liutprando08 <liutprando.federico@protonmail.com>
2026-01-24 19:02:37 +0100

    aggiunta commenti

--- c/insertionSort.c
+++ c/insertionSort.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 int main() {
-  int n = 100;
-  int array[n], c, d, t;
+  // dichiarazione grandezza array
 
+  int n = 100;
+
+  // dichiarazione array, counter, decrementa, temporaneo
+  int array[n], c, d, t;
+  // ciclo principale che scorre array
   for (c = 1; c <= n - 1; c++) {
+    // dare valore a decremento in modo tale
+    // di fare comparezione con i precedenti
     d = c;
+    // ciclo per comparazione
     while (d > 0 & array[d - 1] > array[d]) {
+      // scambio dati se minore di precedenti
       t = array[d - 1];
       array[d - 1] = array[d];
       array[d] = t;
@@ -15,7 +23,7 @@ int main() {
   }
 
   printf("array in ordine crescenti\n");
-
+  // stampa dell'array
   for (c = 0; c < n; c++) {
     printf("%d ", array[c]);
   }
