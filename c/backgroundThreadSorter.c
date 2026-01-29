f6c8d902d563d030af486079bde1b775a880ed2d
Liutprando08 <liutprando.federico@protonmail.com>
2026-01-29 22:42:11 +0100

    programma finito

--- c/backgroundThreadSorter.c
+++ c/backgroundThreadSorter.c
@@ -14,10 +14,10 @@ struct rect {
 };
 
 struct rect r[RECT_SIZE];
-
+// creazione variabile lock
 pthread_mutex_t lock;
 
-int areaCamp(void *r1, void *r2) {
+int areaCamp(const void *r1, const void *r2) {
   struct rect tr1 = *(struct rect *)r1;
   struct rect tr2 = *(struct rect *)r2;
 
@@ -26,14 +26,60 @@ int areaCamp(void *r1, void *r2) {
   return area1 > area2 ? 1 : 0;
 }
 
-void sorter(void *p) {
+void *sorter(void *p) {
   for (;;) {
 
     pthread_mutex_lock(&lock);
+    // puntatore all inizio dell array, numrero di membri, dimensione di ogni
+    // singolo elemento, compareazione con areaCamp
     qsort(r, RECT_SIZE, sizeof(struct rect), areaCamp);
     printf("The rect array is\n");
     for (int i = 0; i < RECT_SIZE; i++) {
       printf("%d %d\n", r[i].length, r[i].breadth);
     }
+    pthread_mutex_unlock(&lock);
+    sleep(10);
+    printf("\n");
   }
+}
+
+int main(int argc, char *argv[]) {
+  pthread_t thread;
+  int index = 0;
+
+  r[0].length = 10;
+  r[0].breadth = 2;
+
+  r[1].length = 34;
+  r[1].breadth = 23;
+
+  r[2].length = 32;
+  r[2].breadth = 3;
+
+  r[3].length = 2;
+  r[3].breadth = 45;
+
+  r[4].length = 2;
+  r[4].breadth = 12;
+  // creazione semaforo e inserimento variabile lock con attributi predefiniti
+  pthread_mutex_init(&lock, NULL);
+  // creazione del thread attr predefiniti e inserimento funzione e variabile da
+  // inserire;
+  pthread_create(&thread, NULL, sorter, (void *)NULL);
+
+  for (;;) {
+    struct rect temp_rect;
+    int length, breadth;
+
+    scanf("%d %d", &length, &breadth);
+
+    temp_rect.length = length;
+    temp_rect.breadth = breadth;
+    // blocco thread sorter
+    pthread_mutex_lock(&lock);
+    // blocco funzione sorter
+    r[(index++) % RECT_SIZE] = temp_rect;
+    pthread_mutex_unlock(&lock);
+  }
+  return EXIT_SUCCESS;
 }
