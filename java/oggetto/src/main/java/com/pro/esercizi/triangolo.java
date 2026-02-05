7674c752c72dcbc61fa5895f221ae0a5c4ede7eb
Liutprando08 <132646615+Liutprando08@users.noreply.github.com>
2026-02-05 09:46:40 +0000

    java

---
+++ java/oggetto/src/main/java/com/pro/esercizi/triangolo.java
@@ -0,0 +1,17 @@
+package com.pro.esercizi;
+
+public class triangolo{
+    private int lat1;
+    private int lat2;
+    private int lat3;
+
+    public triangolo(int lat1, int lat2, lat3){
+        this.lat1 = lat1;
+        this.lat2 = lat2;
+        this.lat3 = lat3;
+
+    }
+    public String toString(){
+        return String stampa = "perimetro = "lat1+lat2+lat3 +" Area = " + lat1 * lat2 /2);
+    }
+}
