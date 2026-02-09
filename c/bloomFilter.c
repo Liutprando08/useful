ebe899ad4fcfcf4b4e37e3dbf20531439b42e34a
Liutprando08 <liutprando.federico@protonmail.com>
2026-02-09 13:01:55 +0100

    Conclusine BloomFilter

--- c/bloomFilter.c
+++ c/bloomFilter.c
@@ -21,6 +21,57 @@ bool check_bit(BloomFilter *bf, size_t n) {
 
 size_t hash(const char *str, int seed) {
   size_t hash = 5381 + seed;
+  int c;
+
+  while ((c = *str++)) {
+    hash = ((hash << 5) + hash) + c;
+  }
 
   return hash % FILTER_SIZE_BITS;
+}
+void destroy(BloomFilter *bf) {
+  free(bf->bits);
+  free(bf);
+}
+
+void bloom_add(BloomFilter *bf, const char *str) {
+  for (int i = 0; i < NUM_HASHES; i++) {
+    size_t position = hash(str, i);
+    set_bits(bf, position);
+  }
+}
+bool bloom_check(BloomFilter *bf, const char *str) {
+  for (int i = 0; i < NUM_HASHES; i++) {
+    size_t position = hash(str, i);
+    if (!check_bit(bf, position)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+BloomFilter *create() {
+  BloomFilter *bf = (BloomFilter *)malloc(sizeof(BloomFilter));
+  size_t bytes_needed = (FILTER_SIZE_BITS + 7) / 8;
+  bf->bits = (uint8_t *)calloc(bytes_needed, sizeof(uint8_t));
+  bf->size = FILTER_SIZE_BITS;
+  return bf;
+}
+
+int main(int argc, char *argv[]) {
+  BloomFilter *bf = create();
+  bloom_add(bf, "mela");
+
+  if (bloom_check(bf, "mela")) {
+    printf("si");
+
+  } else {
+    printf("no");
+  }
+  if (bloom_check(bf, "banana")) {
+    printf("si");
+  } else {
+    printf("no");
+  }
+  return EXIT_SUCCESS;
 }
